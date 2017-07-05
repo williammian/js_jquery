5078d210f74c6425eeb6c2a29eea3e6e5d361b83
williammian <william_mian@yahoo.com.br>
2017-07-05 10:37:39 -0300

    estados cidades

---
+++ WebContent/capitulo_08/javascripts/estados_cidades.js
@@ -0,0 +1,54 @@
+$(function(){
+	
+	//var_estados_cidades (array) está no arquivo var_estados_cidades.js
+
+	function lerEstadosCidades(){
+		for(var i = 0; i < 27; i++){
+			var estado = var_estados_cidades[i];
+			console.log(estado.sigla);
+			console.log(estado.nome);
+			var cidades = estado.cidades;
+			for(var j = 0; j < cidades.length; j++){
+				console.log(cidades[j]);
+			}
+		}
+	}
+	
+	
+	//lerEstadosCidades();
+	
+	function carregarComboEstados(){
+		$('#estados').empty();
+				
+		var $estados = $('#estados');
+		
+		for(var i = 0; i < 27; i++){
+			var estado = var_estados_cidades[i];
+			
+			$estados.append("<option value = '" + i + "'>" + estado.nome + "</option>");
+		}
+		
+		carregarComboCidades();
+	}
+	
+	carregarComboEstados();
+	
+	function carregarComboCidades(){
+		$('#cidades').empty();
+		var $cidades = $('#cidades'); 
+		
+		var value_estado = $("#estados").val();
+		
+		var estado = var_estados_cidades[value_estado];
+		
+		var cidades = estado.cidades;
+		
+		for(var i = 0; i < cidades.length; i++){
+			$cidades.append("<option value = '" + i + "'>" + cidades[i] + "</option>");
+		}
+		
+	}
+	
+	$("#estados").change(carregarComboCidades);
+	
+});
